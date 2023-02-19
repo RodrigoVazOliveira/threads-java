8b56d780301d95d07f35cf6f0f29d7afd0ebf86d
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-02-18 23:56:02 -0300

    read files wih threads

--- src/dev/rvz/search/ApplicationSearchNameMain.java
+++ src/dev/rvz/search/ApplicationSearchNameMain.java
@@ -3,8 +3,16 @@ package dev.rvz.search;
 public class ApplicationSearchNameMain {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	String name = "da";
+	Thread taskSearchAssignatureOne = new Thread(new TaskSearchName("assinaturas1.txt", name),
+		"Thread search name");
+	Thread taskSearchAssignatureTwo = new Thread(new TaskSearchName("assinaturas2.txt", name),
+		"Thread search name");
+	Thread taskSearchAuthors = new Thread(new TaskSearchName("autores.txt", name), "Thread search name");
 
+	taskSearchAssignatureOne.start();
+	taskSearchAssignatureTwo.start();
+	taskSearchAuthors.start();
     }
 
 }
---
+++ src/dev/rvz/search/TaskSearchName.java
@@ -0,0 +1,37 @@
+package dev.rvz.search;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public final class TaskSearchName implements Runnable {
+
+    private final String nameFile;
+    private final String name;
+
+    public TaskSearchName(String nameFile, String name) {
+	super();
+	this.nameFile = nameFile;
+	this.name = name;
+    }
+
+    @Override
+    public void run() {
+	try {
+	    Scanner scanner = new Scanner(new File(nameFile));
+	    Integer numberLine = 1;
+	    while (scanner.hasNextLine()) {
+		String line = scanner.nextLine();
+		if (line.toLowerCase().contains(name.toLowerCase())) {
+		    System.out.println(nameFile + " - " + numberLine + " - " + line);
+		}
+		numberLine++;
+	    }
+	    scanner.close();
+	} catch (FileNotFoundException e) {
+	    throw new RuntimeException(e);
+	}
+
+    }
+
+}
