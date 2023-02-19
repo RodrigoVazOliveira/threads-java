d89061a2da0f13f39edbadd1065c6dd50aa4bedd
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-02-19 00:00:12 -0300

    chagne indentation

--- src/dev/rvz/search/TaskSearchName.java
+++ src/dev/rvz/search/TaskSearchName.java
@@ -6,32 +6,31 @@ import java.util.Scanner;
 
 public final class TaskSearchName implements Runnable {
 
-    private final String nameFile;
-    private final String name;
+	private final String nameFile;
+	private final String name;
 
-    public TaskSearchName(String nameFile, String name) {
-	super();
-	this.nameFile = nameFile;
-	this.name = name;
-    }
-
-    @Override
-    public void run() {
-	try {
-	    Scanner scanner = new Scanner(new File(nameFile));
-	    Integer numberLine = 1;
-	    while (scanner.hasNextLine()) {
-		String line = scanner.nextLine();
-		if (line.toLowerCase().contains(name.toLowerCase())) {
-		    System.out.println(nameFile + " - " + numberLine + " - " + line);
-		}
-		numberLine++;
-	    }
-	    scanner.close();
-	} catch (FileNotFoundException e) {
-	    throw new RuntimeException(e);
+	public TaskSearchName(String nameFile, String name) {
+		this.nameFile = nameFile;
+		this.name = name;
 	}
 
-    }
+	@Override
+	public void run() {
+		try {
+			Scanner scanner = new Scanner(new File(nameFile));
+			Integer numberLine = 1;
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				if (line.toLowerCase().contains(name.toLowerCase())) {
+					System.out.println(nameFile + " - " + numberLine + " - " + line);
+				}
+				numberLine++;
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
 
 }
