9250e2362c9e3489f6e6d6e2e821afd1d8d54a5f
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-02-20 03:42:43 -0300

    add pool threads

--- src/dev/rvz/servers/ServerMain.java
+++ src/dev/rvz/servers/ServerMain.java
@@ -3,20 +3,21 @@ package dev.rvz.servers;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 final class ServerMain {
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("--------- Iniciando servidor --------");
-		ServerSocket serverSocket = new ServerSocket(12345);
+		final ServerSocket serverSocket = new ServerSocket(12345);
+		final ExecutorService executorService = Executors.newCachedThreadPool();
 
 		while (true) {
 			final Socket socket = serverSocket.accept();
 			System.out.println("Aceitando nova conexão - porta: " + socket.getPort());
 			final DistributeConnection distributeConnection = new DistributeConnection(socket);
-			final Thread taskNewConnection = new Thread(distributeConnection);
-			taskNewConnection.start();
-
+			executorService.execute(distributeConnection);
 		}
 	}
 
