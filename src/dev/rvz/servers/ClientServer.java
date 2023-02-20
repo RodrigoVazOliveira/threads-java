46c9f2e88dd0d7f6fd4df8f8d920676b958077f6
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-02-20 03:26:43 -0300

    send info to server socket

--- src/dev/rvz/servers/ClientServer.java
+++ src/dev/rvz/servers/ClientServer.java
@@ -1,6 +1,8 @@
 package dev.rvz.servers;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
@@ -10,9 +12,14 @@ final class ClientServer {
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		final Socket socket = new Socket("localhost", 12345);
 		System.out.println("Conexão estabelecida!");
+		final OutputStream outputStream = socket.getOutputStream();
+		final PrintStream printStream = new PrintStream(outputStream);
+		printStream.println("C1");
 		final Scanner scanner = new Scanner(System.in);
 		scanner.nextLine();
 
+		outputStream.close();
+		scanner.close();
 		socket.close();
 	}
 
--- src/dev/rvz/servers/DistributeConnection.java
+++ src/dev/rvz/servers/DistributeConnection.java
@@ -1,6 +1,9 @@
 package dev.rvz.servers;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 final class DistributeConnection implements Runnable {
 
@@ -14,8 +17,20 @@ final class DistributeConnection implements Runnable {
 	public void run() {
 		System.out.println("Distribuindo as tarefas para o cliente " + socket);
 		try {
+			final InputStream inputStream = socket.getInputStream();
+			final Scanner scanner = new Scanner(inputStream);
+
+			while (scanner.hasNextLine()) {
+				final String line = scanner.nextLine();
+				System.out.println(line);
+			}
+
+			scanner.close();
 			Thread.sleep(20000);
 		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
