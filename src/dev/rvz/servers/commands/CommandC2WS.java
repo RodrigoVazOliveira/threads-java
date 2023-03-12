8de46a45f985cb51b20f6b3bab2bd48d64537e85
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-03-12 20:12:58 -0300

    Ajustar time

--- src/dev/rvz/servers/commands/CommandC2WS.java
+++ src/dev/rvz/servers/commands/CommandC2WS.java
@@ -15,7 +15,7 @@ public final class CommandC2WS implements Callable<String> {
 	public String call() throws Exception {
 		this.printStream.println("Executando comando C2");
 		this.printStream.println("Processamento comando C2 WS");
-		Thread.sleep(20000);
+		Thread.sleep(10000);
 		Integer number = new Random().nextInt(100) + 1;
 		this.printStream.println("Servidor finalizou o comando C2WS!");
 
