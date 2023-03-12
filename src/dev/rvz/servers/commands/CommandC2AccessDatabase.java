4f956a86159b1c896d94df083133423d826858a8
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-03-12 19:53:51 -0300

    Criar comando para acessar banco de dados via thread

---
+++ src/dev/rvz/servers/commands/CommandC2AccessDatabase.java
@@ -0,0 +1,23 @@
+package dev.rvz.servers.commands;
+
+import java.io.PrintStream;
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public final class CommandC2AccessDatabase implements Callable<String> {
+	private final PrintStream printStream;
+
+	public CommandC2AccessDatabase(PrintStream printStream) {
+		this.printStream = printStream;
+	}
+
+	@Override
+	public String call() throws Exception {
+		this.printStream.println("Servidor recebeu comando C2 - ACESSO A BASE DE DADOS");
+		this.printStream.println("Processando comando C2 - banco de dados");
+		Thread.sleep(25000);
+		Integer number = new Random().nextInt(100) + 1;
+		return number.toString();
+	}
+
+}
