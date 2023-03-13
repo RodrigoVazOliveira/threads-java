3edcdc35d4162282302ff7cfd1136e59821c48e7
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-03-12 22:24:46 -0300

    Testar usando bloqueante

--- src/dev/rvz/queues/TestQueueApplication.java
+++ src/dev/rvz/queues/TestQueueApplication.java
@@ -7,9 +7,15 @@ public class TestQueueApplication {
 
 	public static void main(String[] args) throws InterruptedException {
 		BlockingQueue<String> blockingQueueString = new ArrayBlockingQueue<>(3);
-		blockingQueueString.offer("C1");
-		blockingQueueString.offer("C2");
-		blockingQueueString.offer("C3");
+		blockingQueueString.put("C1");
+		blockingQueueString.put("C2");
+		blockingQueueString.put("C3");
+
+		System.out.println(blockingQueueString.take());
+		System.out.println(blockingQueueString.take());
+
+		blockingQueueString.put("C4");
+		blockingQueueString.put("C5");
 
 		System.out.println(blockingQueueString.take());
 		System.out.println(blockingQueueString.take());
