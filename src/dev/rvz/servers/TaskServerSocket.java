3d5d035333dc20663a47985bdb2e3315dae025c4
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-03-12 22:56:40 -0300

    Adicionar queue no contrato da distribuicao

--- src/dev/rvz/servers/TaskServerSocket.java
+++ src/dev/rvz/servers/TaskServerSocket.java
@@ -3,6 +3,8 @@ package dev.rvz.servers;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -12,6 +14,7 @@ final class TaskServerSocket {
 	private final Integer PORT = 12345;
 	private final ExecutorService executorService;
 	private AtomicBoolean isRunning;
+	private final BlockingQueue<String> queueCommand;
 
 	public TaskServerSocket() throws IOException {
 		System.out.println("--------- Iniciando servidor --------");
@@ -19,13 +22,24 @@ final class TaskServerSocket {
 		final ThreadFactoryCustom threadFactoryCustom = new ThreadFactoryCustom();
 		this.executorService = Executors.newCachedThreadPool(threadFactoryCustom);
 		this.isRunning = new AtomicBoolean(true);
+		this.queueCommand = new ArrayBlockingQueue<>(2);
+		startConsummers();
+	}
+
+	private void startConsummers() {
+		final Integer totalConsummers = 2;
+		for (int i = 0; i < totalConsummers; i++) {
+			final TaskConsummer taskConsummer = new TaskConsummer(queueCommand);
+			this.executorService.execute(taskConsummer);
+		}
 	}
 
 	public void run() throws IOException {
 		while (this.isRunning.get()) {
 			final Socket socket = this.serverSocket.accept();
 			System.out.println("Aceitando nova conexão - porta: " + socket.getPort());
-			final DistributeConnection distributeConnection = new DistributeConnection(executorService, socket, this);
+			final DistributeConnection distributeConnection = new DistributeConnection(executorService, socket, this,
+					this.queueCommand);
 			executorService.execute(distributeConnection);
 		}
 	}
